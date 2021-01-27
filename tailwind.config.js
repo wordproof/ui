5c2cf691044c112def91861203b5e54ec9e76dc9
Vasyl <vasyl@wordproof.com>
2021-01-27 21:40:42 +0200

    add: custom wordproof colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#252525',
+        'white': '#FFFFFF',
+        'blue': '#2000FF',
+        'pink': '#FF1F7C',
+        'teal': '#00E8C6',
+        'yellow': '#FFA713',
+        'purple': '#783CDC',
+        'gray': {
+          200: '#F5F7FA',
+          400: '#E3E2E6',
+          600: '#AEA9AB',
+          800: '#5f5d5e',
+        },
+        'light-blue': '#E2E9F4',
+        'sand': '#FFEBCA',
+      },
+    },
   },
   variants: {
     extend: {},
