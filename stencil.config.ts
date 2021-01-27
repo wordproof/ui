9742f4d316dd219a86e20dbc01a33b222847d9c3
Vasyl <vasyl@wordproof.com>
2021-01-27 13:25:48 +0200

    add: postcss and purgecss settings

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,4 +1,11 @@
 import { Config } from '@stencil/core';
+import { postcss } from '@stencil/postcss';
+import autoprefixer from 'autoprefixer';
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.tsx', './src/**/*.css', './src/index.html'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 export const config: Config = {
   namespace: 'uikit',
@@ -17,5 +24,17 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+  ],
+  globalStyle: 'src/global/styles.css',
+  plugins: [
+    postcss({
+      plugins: [
+        require('tailwindcss')('./tailwind.config.js'),
+        autoprefixer(),
+        ...(process.env.NODE_ENV === 'production'
+          ? [purgecss, require('cssnano')]
+          : []),
+      ],
+    }),
   ],
 };
