ad095787cbeb5ace46452574698cdef385311ded
Vasyl <vasyl@wordproof.com>
2021-01-28 20:49:04 +0200

    add: configure vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'WordProof UI Kit',
+  head: [
+    [
+      'script',
+      {
+        type: 'module',
+        src: `https://unpkg.com/@wordproof/uikit/dist/uikit/uikit.esm.js`,
+      },
+    ],
+  ],
+};
