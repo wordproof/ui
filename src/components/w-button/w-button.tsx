5a836b5a07b3f63a7f3ece51563cfda671a11e00
Vasyl <vasyl@wordproof.com>
2021-01-28 13:09:15 +0200

    add: sohne fonts styles

--- src/components/w-button/w-button.tsx
+++ src/components/w-button/w-button.tsx
@@ -26,7 +26,8 @@ export class WButton {
     return (
       <button
         type={this.type}
-        class="inline-flex items-center px-5 py-2 bg-gradient-to-r from-blue to-purple border border-transparent rounded-full text-white hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-blue focus:shadow-outline-blue transition ease-in-out duration-150">
+        class="font-sohne-bold inline-flex items-center px-5 py-2 bg-gradient-to-r from-blue to-purple border border-transparent rounded-full text-white hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-blue focus:shadow-outline-blue transition ease-in-out duration-150"
+      >
         <slot></slot>
       </button>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: false,
   darkMode: false, // or 'media' or 'class'
@@ -19,6 +21,29 @@ module.exports = {
         },
         'light-blue': '#E2E9F4',
         'sand': '#FFEBCA',
+      },
+      fontSize: {
+        'xs': '.75rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.5rem',
+        '2xl': '2rem',
+        '3xl': '2.5rem',
+      },
+      spacing: {
+        1: '4px',
+        2: '8px',
+        3: '12px',
+        4: '16px',
+        6: '24px',
+        8: '32px',
+        10: '40px',
+        20: '80px',
+      },
+      fontFamily: {
+        'sans': ['Nunito', ...defaultTheme.fontFamily.sans],
+        'sohne-bold': ['Sohne-Dreiviertelfett'],
+        'sohne': ['Sohne-Sohne-Buch'],
       },
     },
   },
