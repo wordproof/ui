92bdc2f479065df8fa9beda165c194ecf7bcdb66
Vasyl <vasyl@wordproof.com>
2021-02-22 20:35:37 +0200

    add: compare date select logic

--- src/components/w-certificate/views/w-certificate-compare-view/w-certificate-compare-view.tsx
+++ src/components/w-certificate/views/w-certificate-compare-view/w-certificate-compare-view.tsx
@@ -1,9 +1,13 @@
-import { Component, Prop, h, Element, State } from '@stencil/core';
+import { Component, Prop, h, Element, State, Watch } from '@stencil/core';
 import { CertificateStrings } from '../../../../i18n';
 import { router } from '../../../w-router-outlet';
 import Button from '../../components/Button';
-import { WPContent } from '../../service';
+import { WPContent, WPRevision } from '../../service';
 
+interface RevisionOption {
+  label: string;
+  value: number;
+}
 @Component({
   tag: 'w-certificate-compare-view',
   shadow: true,
@@ -22,7 +26,7 @@ export class WCertificateLink {
     if (ind === 0) {
       prefix = `${this.strings.recent} `;
     }
-    if (ind === this.content.revisions.length - 1) {
+    if (ind === this.content.revisions.length) {
       prefix = `${this.strings.firstTimestamp} `;
     }
 
@@ -31,7 +35,8 @@ export class WCertificateLink {
       month: '2-digit',
       day: '2-digit',
       hour: 'numeric',
-      minute: 'numeric',
+      minute: '2-digit',
+      second: '2-digit',
     });
 
     return `${prefix}${date}`;
@@ -45,8 +50,40 @@ export class WCertificateLink {
       .replace(new RegExp(PARAGRAPH_DIVIDER, 'gi'), '<br /><br />');
   }
 
-  @State() selectedLeft: number = 0;
-  @State() selectedRight: number = 1;
+  @State() oldRevisionValue: number;
+
+  @State() newRevisionValue: number;
+
+  @Watch('oldRevisionValue')
+  watchOldRevisionValue(newValue: number) {
+    this.newOptions = this.allOptions.filter(option => option.value < newValue);
+  }
+
+  @Watch('newRevisionValue')
+  watchNewRevisionValue(newValue: number) {
+    this.oldOptions = this.allOptions.filter(option => option.value > newValue);
+  }
+
+  allRevisions: WPRevision[];
+  allOptions: RevisionOption[];
+
+  @State() oldOptions: RevisionOption[];
+
+  @State() newOptions: RevisionOption[];
+
+  componentWillLoad() {
+    const { revisions, ...otherProps } = this.content;
+
+    this.allRevisions = [otherProps, ...revisions];
+
+    this.allOptions = this.allRevisions.map((revision, ind) => ({
+      label: `${this.formatOptionLabel(revision.date, ind)}`,
+      value: ind,
+    }));
+
+    this.oldRevisionValue = 1;
+    this.newRevisionValue = 0;
+  }
 
   render() {
     return (
@@ -60,22 +97,31 @@ export class WCertificateLink {
 
         <div>
           <div class="flex flex-col sm:flex-row sm:space-x-4 mx-4 mt-3 mb-4">
-            <w-input-select value={this.selectedLeft} class="w-full">
-              {this.content.revisions.map((revision, ind) => (
+            <w-input-select
+              value={this.oldRevisionValue}
+              class="w-full"
+              onInput={(ev: InputEvent) => {
+                this.oldRevisionValue = Number(ev.data);
+              }}
+            >
+              {this.oldOptions.map(option => (
                 <w-input-select-option
-                  value={ind}
-                  label={this.formatOptionLabel(revision.date, ind)}
+                  value={option.value}
+                  label={option.label}
                 ></w-input-select-option>
               ))}
             </w-input-select>
             <w-input-select
-              value={this.selectedRight}
+              value={this.newRevisionValue}
               class="w-full my-2 sm:my-0"
+              onInput={(ev: InputEvent) => {
+                this.newRevisionValue = Number(ev.data);
+              }}
             >
-              {this.content.revisions.map((revision, ind) => (
+              {this.newOptions.map(option => (
                 <w-input-select-option
-                  value={ind}
-                  label={this.formatOptionLabel(revision.date, ind)}
+                  value={option.value}
+                  label={option.label}
                 ></w-input-select-option>
               ))}
             </w-input-select>
@@ -89,7 +135,7 @@ export class WCertificateLink {
               <div
                 class="w-full break-all"
                 innerHTML={this.cleanUp(
-                  this.content.revisions[this.selectedRight].content,
+                  this.allRevisions[this.oldRevisionValue].content,
                 )}
               ></div>
             </div>
@@ -100,7 +146,7 @@ export class WCertificateLink {
               <div
                 class="w-full break-all"
                 innerHTML={this.cleanUp(
-                  this.content.revisions[this.selectedRight].content,
+                  this.allRevisions[this.newRevisionValue].content,
                 )}
               ></div>
             </div>
