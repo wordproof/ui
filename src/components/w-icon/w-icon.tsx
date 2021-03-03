c3f07a8c6fc54c9b7076532d707aba31e867ca79
Vasyl <vasyl@wordproof.com>
2021-03-03 19:44:27 +0200

    add: hamburger icons

--- src/components/w-icon/w-icon.tsx
+++ src/components/w-icon/w-icon.tsx
@@ -149,6 +149,26 @@ export class WIcon {
           />
         </symbol>
 
+        <symbol id="hamburger" viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </symbol>
+
+        <symbol id="hamburger-close" viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </symbol>
+
         <symbol id="somename" viewBox="0 0 28 23"></symbol>
       </svg>
     );
