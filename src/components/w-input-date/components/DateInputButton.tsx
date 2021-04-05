a54cfc25903e234f841ae9c17da52bce6ae64a18
Vasyl <vasyl@wordproof.com>
2021-04-05 16:41:41 +0300

    add: date input button

---
+++ src/components/w-input-date/components/DateInputButton.tsx
@@ -0,0 +1,56 @@
+import { FunctionalComponent, h } from '@stencil/core';
+import { format, isSameDay } from 'date-fns';
+import { parseDate } from '../../../utils/date';
+import cx from 'classnames';
+
+interface DateInputButtonProps {
+  dateStr?: string;
+  onClick?: Function;
+}
+
+const getButtonText = (dateStr: string): string => {
+  const date = parseDate(dateStr);
+
+  if (isSameDay(date, new Date())) {
+    return 'Today’s version';
+  }
+
+  if (dateStr === '') {
+    return 'Select day to compare';
+  }
+
+  return format(date, 'MMMM d, yyyy');
+};
+
+const DateInputButton: FunctionalComponent<DateInputButtonProps> = ({
+  dateStr = '',
+  onClick = () => {},
+}) => (
+  <button
+    class={cx(
+      'flex pl-4 pr-7 py-3 items-center rounded-full focus:outline-none focus:ring-2',
+      {
+        'bg-gradient-to-r from-blue to-purple text-white':
+          dateStr === '',
+        'bg-white shadow text-blue': dateStr !== '',
+      },
+    )}
+    onClick={() => {
+      onClick();
+    }}
+  >
+    <div class="p-1.5 bg-blue border border-blue rounded-full">
+      <w-icon name="calendar" class="text-black"></w-icon>
+    </div>
+    <div
+      class={cx('ml-4', {
+        'font-sohne-semibold': dateStr === '',
+        'font-sohne border-b border-blue': dateStr !== '',
+      })}
+    >
+      {getButtonText(dateStr)}
+    </div>
+  </button>
+);
+
+export default DateInputButton;
