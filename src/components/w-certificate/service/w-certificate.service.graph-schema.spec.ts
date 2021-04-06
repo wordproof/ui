58c64239264c59736a44a850257186fe334d131c
Vasyl <vasyl@wordproof.com>
2021-04-06 20:12:03 +0300

    add: w-certificate e2e test

--- src/components/w-certificate/service/w-certificate.service.graph-schema.spec.ts
+++ src/components/w-certificate/service/w-certificate.service.graph-schema.spec.ts
@@ -13,7 +13,7 @@ const hashLinkContent = {
     '<h1>Rechtspraak niet beter na hervorming</h1>\n\n<p>De vermindering van het aantal rechtbanken, parketten van het Openbaar Ministerie en gerechtshoven in 2013 heeft de rechtspraak niet aanwijsbaar verbeterd. Dat concluderen vier hoogleraren en een burgemeester (van Gouda) na twee jaar onderzoek naar deze zogenoemde ‘herziening van de gerechtelijke kaart’. Zij leverden hun evaluatie maandag in bij minister Dekker (Rechtsbescherming, VVD).</p>\n<p>In totaal verdwenen vier jaar geleden acht rechtbanken en negen regiokantoren van het Openbaar Ministerie (OM). De vijf verschillende gerechtshoven werden tot één landelijk parket samengevoegd. </p>\n<p>De spreiding van de rechtspraak is daardoor verminderd. De kwaliteit moest beter worden, doordat grotere rechtbanken meer specialistische kennis hebben. De hoogleraren hebben dat effect niet kunnen meten. Ze stellen wel vast dat het ministerie signalen heeft „gemist of onvoldoende gedeeld” dat de rechtspraak sinds de hervorming minder toegankelijk is.</p>\n<p>President Fred van der Winkel van het gerechtshof Arnhem/Leeuwarden zei maandag in <em>De Gelderlander</em> dat er te veel ingewikkelde zaken zijn voor Arnhem/Leeuwarden. Amsterdamse collega’s gaan in 2018 in Utrecht zaken namens zijn gerechtshof afdoen.</p>\n<p>Personeel van rechtsprekende instanties en het OM beoordelen de hervorming met een 5,3. De evaluatiecommissie noemt  het „spijtig” dat de herziening is beleefd als bezuiniging. Nieuwe hervorming is volgens de hoogleraren niet nodig – wel bijsturing en „meer tijd”. De minister reageert komend voorjaar.</p>',
 };
 
-const scriptTagContent = {
+export const scriptTagContent = {
   '@context': 'https://schema.org',
   '@graph': [
     {
--- src/components/w-certificate/w-certificate.e2e.ts
+++ src/components/w-certificate/w-certificate.e2e.ts
@@ -1,4 +1,5 @@
 import { newE2EPage } from '@stencil/core/testing';
+import { scriptTagContent } from './service/w-certificate.service.graph-schema.spec';
 import { NO_DATA_CERTIFICATE_COMMENT_NODE_TEXT } from './types';
 
 describe('w-certificate', () => {
@@ -10,7 +11,7 @@ describe('w-certificate', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders a comment node inside shadow DOM if there are NO certificate data on the page', async () => {
+  it('renders a comment node inside shadow DOM if there is NO certificate data on the page', async () => {
     const page = await newE2EPage();
 
     await page.setContent('<w-certificate></w-certificate>');
@@ -18,5 +19,21 @@ describe('w-certificate', () => {
     const commentText = element.shadowRoot.host.childNodes[0]['_nodeValue'];
 
     expect(commentText).toEqual(NO_DATA_CERTIFICATE_COMMENT_NODE_TEXT);
+  });
+
+  it('renders w-certificate-link and w-modal elements inside shadow DOM if there is graph schema data on the page', async () => {
+    const page = await newE2EPage();
+
+    await page.setContent(/*html*/ `
+      <script type="application/ld+json" data-json-ld-for-pagemetadata>
+        ${JSON.stringify(scriptTagContent)}
+      </script>
+      <w-certificate></w-certificate>
+    `);
+    const linkElement = await page.find('w-certificate >>> w-certificate-link');
+    const modalElement = await page.find('w-certificate >>> w-modal');
+
+    expect(linkElement).toBeDefined();
+    expect(modalElement).toBeDefined();
   });
 });
