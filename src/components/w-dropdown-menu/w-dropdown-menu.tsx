f9b0eeef0be260ab2b3fcdb6d5725531d8fe0f95
Vasyl <vasyl@wordproof.com>
2021-04-09 18:17:17 +0300

    add: close on esc

--- src/components/w-dropdown-menu/w-dropdown-menu.tsx
+++ src/components/w-dropdown-menu/w-dropdown-menu.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h, State } from '@stencil/core';
+import { Component, Prop, h, State, Listen } from '@stencil/core';
 import cx from 'classnames';
 
 export interface DropdownMenuOption {
@@ -20,12 +20,28 @@ export class WDropdownMenu {
 
   @State() show: boolean = false;
 
+  triggerButtonElement: HTMLButtonElement;
+
+  @Listen('keydown')
+  handleKeyDown(ev: KeyboardEvent) {
+    if (ev.key === 'Escape') {
+      ev.stopPropagation();
+      this.close();
+    }
+  }
+
+  close() {
+    this.show = false;
+    this.triggerButtonElement.blur();
+  }
+
   render() {
     return (
       <div class="">
         <button
           class="rounded-full focus:outline-none focus:ring"
-          onClick={() => (this.show = !this.show)}
+          ref={el => (this.triggerButtonElement = el as HTMLButtonElement)}
+          onClick={() => (this.show = true)}
         >
           <slot name="trigger">
             <w-icon
@@ -41,13 +57,14 @@ export class WDropdownMenu {
         >
           <div
             class="fixed w-screen h-screen top-0 left-0 bg-transparent z-20"
-            onClick={() => (this.show = false)}
+            onClick={() => this.close()}
           ></div>
-          <ul class={cx('absolute right-10 mt-1 bg-white border-light-blue divide-y rounded shadow-md z-30', {})}>
+          <ul class="absolute right-10 mt-1 bg-white border-light-blue divide-y rounded shadow-md z-30">
             {this.options.map(option => (
               <li
-                class="px-6 pt-5 pb-6 whitespace-nowrap select-none hover:bg-gray-200 cursor-pointer"
+                class="pl-6 pr-12 pt-5 pb-6 whitespace-nowrap select-none hover:bg-gray-200 cursor-pointer"
                 onClick={() => {
+                  this.close();
                   if (option.action) {
                     option.action();
                   }
