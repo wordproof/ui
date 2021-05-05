d4adcae8506b4bd2c0d27a162b3cd751bfb7a455
Vasyl <vasyl@wordproof.com>
2021-05-05 20:02:43 +0300

    fix: remove legend buttons

--- src/components/w-certificate-v4/components/ContentPreview.tsx
+++ src/components/w-certificate-v4/components/ContentPreview.tsx
@@ -30,73 +30,55 @@ const ContentPreview: FunctionalComponent<ContentPreviewProps> = ({
   viewInd,
   diffInd,
   classes = '',
-  strings,
 }) => {
   return (
     <div
-      class={cx('w-full rounded border flex', {
+      class={cx('w-full rounded border relative', {
         'border-light-blue bg-white': view !== 'raw',
         'bg-black': view === 'raw',
       })}
     >
-      <div class="w-40 px-4 pt-10 pb-6 flex">
-        {view === 'diff' ? (
-          <div class="flex flex-wrap content-evenly">
-            <LegendButton color="blue" label={strings.changed} />
-            <LegendButton color="pink" label={strings.removed} />
-          </div>
-        ) : null}
-      </div>
-      <div class="w-full relative">
+      <div
+        class={cx('absolute h-8 rounded', {
+          'bg-white': view !== 'raw',
+          'bg-black': view === 'raw',
+        })}
+        style={{ width: 'calc(100% - 24px)' }}
+      ></div>
+      {view === 'raw' || view === 'clean' ? (
+        <textarea
+          readonly
+          class={cx(
+            `rounded font-sohne resize-none block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll text-black focus:outline-none ${classes}`,
+            {
+              'bg-white text-black': view !== 'raw',
+              'bg-black text-white font-mono': view === 'raw',
+            },
+          )}
+        >
+          {view === 'clean' ? renderContent(revisions, 'clean', viewInd) : null}
+          {view === 'raw' ? renderContent(revisions, 'raw', viewInd) : null}
+        </textarea>
+      ) : null}
+      {view === 'render' ? (
         <div
-          class={cx('absolute w-full h-10 right-5', {
-            'bg-white': view !== 'raw',
-            'bg-black': view === 'raw',
-          })}
+          class="block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll"
+          innerHTML={renderContent(revisions, view as 'render', viewInd)}
         ></div>
+      ) : null}
+      {view === 'diff' && diffInd !== undefined ? (
         <div
-          class={cx('absolute bottom-0 w-full h-6 right-5', {
-            'bg-white': view !== 'raw',
-            'bg-black': view === 'raw',
-          })}
+          class="block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll font-sohne"
+          innerHTML={renderContent(
+            revisions,
+            view as 'diff',
+            viewInd,
+            diffInd,
+            styleAsAdded,
+            styleAsRemoved,
+          )}
         ></div>
-        {view === 'raw' || view === 'clean' ? (
-          <textarea
-            readonly
-            class={cx(
-              `font-sohne resize-none block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll text-black focus:outline-none ${classes}`,
-              {
-                'bg-white text-black': view !== 'raw',
-                'bg-black text-white font-mono': view === 'raw',
-              },
-            )}
-          >
-            {view === 'clean'
-              ? renderContent(revisions, 'clean', viewInd)
-              : null}
-            {view === 'raw' ? renderContent(revisions, 'raw', viewInd) : null}
-          </textarea>
-        ) : null}
-        {view === 'render' ? (
-          <div
-            class="block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll"
-            innerHTML={renderContent(revisions, view as 'render', viewInd)}
-          ></div>
-        ) : null}
-        {view === 'diff' && diffInd !== undefined ? (
-          <div
-            class="block w-full h-80 max-w-full pt-10 pb-8 px-4 overflow-y-scroll font-sohne"
-            innerHTML={renderContent(
-              revisions,
-              view as 'diff',
-              viewInd,
-              diffInd,
-              styleAsAdded,
-              styleAsRemoved,
-            )}
-          ></div>
-        ) : null}
-      </div>
+      ) : null}
     </div>
   );
 };
