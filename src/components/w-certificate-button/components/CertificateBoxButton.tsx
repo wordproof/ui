97837b5ae0d6b609b806b9e8703224ca3cbc3294
Vasyl <vasyl@wordproof.com>
2021-05-20 19:12:50 +0300

    fix: box fluid button mobile layout

--- src/components/w-certificate-button/components/CertificateBoxButton.tsx
+++ src/components/w-certificate-button/components/CertificateBoxButton.tsx
@@ -27,16 +27,20 @@ const CertificateBoxButton: FunctionalComponent<CertificateBoxButtonProps> = ({
   variant,
 }) => (
   <button
-    class={cx(ButtonDefaultClasses(), 'shadow focus:ring-blue flex-col sm:flex-row', {
-      ['px-8 pt-3 pb-1 sm:py-11 rounded-sm shape-left shape-base']:
-        variant === 'base',
-      ['sm:pl-10 sm:pr-9 px-4 pt-3 pb-1 sm:pt-6 sm:pb-5 rounded-3xl sm:rounded-full shape-right shape-rounded']:
-        variant === 'rounded',
-      ['px-12 py-2 sm:py-6 rounded-sm']: variant === 'sm',
-      ['px-12 pt-6 pb-4 rounded-sm flex-col shape-left shape-tall']:
-        variant === 'tall',
-      ['pl-7 pr-10 py-5 rounded-sm w-full']: variant === 'fluid',
-    })}
+    class={cx(
+      ButtonDefaultClasses(),
+      'shadow focus:ring-blue flex-col sm:flex-row',
+      {
+        ['px-8 pt-3 pb-1 sm:py-11 rounded-sm shape-left shape-base']:
+          variant === 'base',
+        ['sm:pl-10 sm:pr-9 px-4 pt-3 pb-1 sm:pt-6 sm:pb-5 rounded-3xl sm:rounded-full shape-right shape-rounded']:
+          variant === 'rounded',
+        ['px-12 py-2 sm:py-6 rounded-sm']: variant === 'sm',
+        ['px-12 pt-6 pb-4 rounded-sm flex-col shape-left shape-tall']:
+          variant === 'tall',
+        ['pl-7 pr-10 py-3 sm:py-5 rounded-sm w-full']: variant === 'fluid',
+      },
+    )}
     type="button"
     onClick={ev => onClick(ev)}
     style={{ '--tw-ring-opacity': '0.5' }}
@@ -47,7 +51,7 @@ const CertificateBoxButton: FunctionalComponent<CertificateBoxButtonProps> = ({
       class={cx('flex flex-col', {
         ['mt-4 items-center']: variant === 'tall',
         ['items-left']: variant !== 'tall',
-        ['ml-3 mt-1']: variant === 'fluid',
+        ['ml-3 mt-2 sm:mt-1']: variant === 'fluid',
       })}
     >
       {variant === 'tall' || variant === 'fluid' ? <TextLogo /> : <RectLogo />}
@@ -68,7 +72,7 @@ const CertificateBoxButton: FunctionalComponent<CertificateBoxButtonProps> = ({
         ['sm:ml-8  text-center mt-2']: variant === 'base',
         ['sm:ml-10 text-center mt-2']: variant === 'rounded',
         ['hidden']: variant === 'sm' || variant === 'tall',
-        ['mx-10 flex-grow']: variant === 'fluid',
+        ['sm:mx-10 flex-grow text-center mt-2']: variant === 'fluid',
       })}
     >
       {text}
@@ -79,7 +83,8 @@ const CertificateBoxButton: FunctionalComponent<CertificateBoxButtonProps> = ({
       class={cx('text-blue', {
         ['hidden']: variant === 'sm',
         ['mt-4']: variant === 'tall',
-        ['my-2 sm:my-0']: variant === 'rounded' || variant === 'base',
+        ['my-2 sm:my-0']:
+          variant === 'rounded' || variant === 'base' || variant === 'fluid',
       })}
     ></w-icon>
   </button>
