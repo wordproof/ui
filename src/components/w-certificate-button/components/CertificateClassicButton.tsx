24abe347ea0186b8c3bf5b54798eb2031a0cffdb
Vasyl <vasyl@wordproof.com>
2021-05-20 13:02:19 +0300

    fix: replace logo

--- src/components/w-certificate-button/components/CertificateClassicButton.tsx
+++ src/components/w-certificate-button/components/CertificateClassicButton.tsx
@@ -16,7 +16,7 @@ const CertificateClassicButton: FunctionalComponent<CertificateClassicButtonProp
 }) => {
   return (
     <button
-      class={cx(ButtonDefaultClasses())}
+      class={cx(ButtonDefaultClasses(), 'focus:ring-1 p-1 rounded')}
       type="button"
       onClick={ev => onClick(ev)}
       style={{
@@ -25,8 +25,26 @@ const CertificateClassicButton: FunctionalComponent<CertificateClassicButtonProp
         '--tw-ring-color': color,
       }}
     >
-      <ShieldLogo color={color} />
-      <span class="ml-3 mt-1px whitespace-nowrap">{text}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 59 59"
+        class="w-6 h-6 mr-1"
+      >
+        <g fill-rule="nonzero" fill="none">
+          <rect fill="#00E8C6" width="59" height="59" rx="2"></rect>
+          <path
+            d="M49.58 15.785l-7.842 28.862a1 1 0 01-.965.738h-6.854L29.954 29.31h6.26l1.566 7.333 6.185-22.12h4.65a1 1 0 01.965 1.261z"
+            fill="#FFF"
+          ></path>
+          <path
+            d="M26.713 14.523l-6.158 22.07-4.75-22.07h-5.452a1 1 0 00-.97 1.24l7.113 28.86a1 1 0 00.971.762h6.062a1 1 0 00.965-.738l7.84-28.862a1 1 0 00-.965-1.262h-4.656z"
+            fill="#FFF"
+            opacity=".8"
+          ></path>
+        </g>
+      </svg>
+
+      <span class="mt-2px whitespace-nowrap">{text}</span>
     </button>
   );
 };
