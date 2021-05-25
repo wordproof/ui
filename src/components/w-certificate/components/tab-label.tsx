b2965719c3bedb488f9a8177c08ba83087024e6d
Vasyl <vasyl@wordproof.com>
2021-05-25 17:26:23 +0300

    add: certificate view

--- src/components/w-certificate/components/tab-label.tsx
+++ src/components/w-certificate/components/tab-label.tsx
@@ -2,20 +2,27 @@ import { FunctionalComponent, h } from '@stencil/core';
 import cx from 'classnames';
 
 interface TabLabelProps {
+  text?: string;
+  topMarginClass?: string;
   onClick: Function;
 }
 
-const TabLabel: FunctionalComponent<TabLabelProps> = ({ onClick }) => (
+const TabLabel: FunctionalComponent<TabLabelProps> = ({
+  onClick,
+  text = 'About WordProof',
+  topMarginClass = 'mt-32',
+}) => (
   <div
     class={cx(
-      'hidden sm:inline-flex select-none bg-gradient-to-b from-blue to-purple text-white font-sohne-semibold rounded-r w-10 pt-3 pb-4 items-center justify-center absolute right-0 top-0 mt-32 transform translate-x-full',
+      'hidden sm:inline-flex select-none bg-gradient-to-b from-blue to-purple text-white font-sohne-semibold rounded-r w-10 pt-3 pb-4 items-center justify-center absolute right-0 top-0 transform translate-x-full',
+      topMarginClass,
     )}
     style={{ writingMode: 'vertical-rl' }}
     onClick={() => onClick()}
     role="button"
   >
     <w-icon name="info" class="mb-3"></w-icon>
-    About WordProof
+    {text}
   </div>
 );
 
--- src/components/w-serp-certificate/w-serp-certificate.tsx
+++ src/components/w-serp-certificate/w-serp-certificate.tsx
@@ -1,5 +1,18 @@
-import { Component, Prop, h, Element } from '@stencil/core';
+import { Component, Prop, h, Element, State } from '@stencil/core';
 import { SerpCertificateStrings } from '../../i18n';
+import { DateTimeFormatOptions, formatDate } from '../../utils/locale';
+import TabLabel from '../w-certificate/components/tab-label';
+import CertificateItem from './components/CertificateItem';
+
+const DATE_FORMAT_OPTIONS = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+} as DateTimeFormatOptions;
+
+const LOCALE = 'en-US';
 
 @Component({
   tag: 'w-serp-certificate',
@@ -10,13 +23,13 @@ export class WSerpCertificate {
   @Element() hostElement: HTMLElement;
 
   strings: SerpCertificateStrings = {
-    timestamped: 'Timestanped',
+    timestamped: 'Timestamped',
     someWebsitesChoose:
       "Some websites choose to anchor the fingerprint of their content on the blockchain. This is called 'timestamping'.",
     withTimestampsYouCan:
       'With timestamps, you can verify if and how information changed over time, and what individual or organization published it.',
     certificate: 'Certificate',
-    whatIsThis: 'What id this?',
+    whatIsThis: 'What is this?',
     firstTimestamped: 'First timestamped',
     lastEdited: 'Last edited',
     publishedBy: 'Published by',
@@ -37,13 +50,65 @@ export class WSerpCertificate {
    */
   @Prop() publishedBy: string = '';
 
+  @State() showCertificate: boolean = true;
+
   render() {
     return (
-      <div>
-        <div>{this.strings.someWebsitesChoose}</div>
-        <div>{this.firstTimestamped}</div>
-        <div>{this.lastEdited}</div>
-        <div>{this.publishedBy}</div>
+      <div class="w-128 h-96 px-12 py-8 rounded-lg bg-white relative flex flex-col justify-items-center">
+        <TabLabel
+          onClick={() => {
+            this.showCertificate = !this.showCertificate;
+          }}
+          text={
+            this.showCertificate
+              ? this.strings.whatIsThis
+              : this.strings.certificate
+          }
+          topMarginClass="mt-24"
+        />
+
+        <div class="flex items-center justify-center">
+          <div class="w-10 h-10 rounded-full flex items-center justify-center bg-gradient-to-r from-blue to-purple text-white">
+            <w-icon fit class="w-6 h-6" name="shield"></w-icon>
+          </div>
+          <h2 class="ml-4 text-xl font-sohne-semibold font-semibold text-black">
+            {this.strings.timestamped}
+          </h2>
+        </div>
+
+        <div class="shadow-md rounded mx-auto mt-8 px-6 font-sohne bg-white w-full divide-y divide-gray-400">
+          <CertificateItem
+            label={this.strings.firstTimestamped}
+            text={formatDate(
+              this.firstTimestamped,
+              LOCALE,
+              DATE_FORMAT_OPTIONS,
+            )}
+            checked={true}
+          />
+
+          <CertificateItem
+            label={this.strings.lastEdited}
+            text={formatDate(this.lastEdited, LOCALE, DATE_FORMAT_OPTIONS)}
+            checked={true}
+          />
+
+          <CertificateItem
+            label={this.strings.publishedBy}
+            text={this.publishedBy}
+            checked={true}
+          />
+        </div>
+
+        <div class="flex-grow flex items-end justify-center">
+          <w-logo
+            fit
+            text
+            whiteOnBlue
+            textLarge
+            class="h-6 inline-flex"
+          ></w-logo>
+        </div>
       </div>
     );
   }
